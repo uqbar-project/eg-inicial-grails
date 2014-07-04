2d3caec83fe04cd041c544c8f29c27f8065ffd73
Fernando Dodino <fernando.dodino@gmail.com>
2014-07-04 11:18:28 +0000

    Chau println

--- grails-app/controllers/ar/edu/calculadora/CalculadoraController.groovy
+++ grails-app/controllers/ar/edu/calculadora/CalculadoraController.groovy
@@ -11,14 +11,12 @@ class CalculadoraController {
 	def calculadora = new Calculadora()
 	
 	def index() { 
-		println "index - numero1: " + calculadora.numero1
 		[calculadora: calculadora]
 	}
 	
 	def sumar() {
 		calculadora.numero1 = new BigDecimal(params.numero1)
 		calculadora.numero2 = new BigDecimal(params.numero2)
-		println "sumar - numero1: " + calculadora.numero1
 		calculadora.sumar()
 		[resultado: calculadora.resultado]
 	}
